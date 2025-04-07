cf9a1750aaf5cbe912a9db5a6428126883bbd788
genadigeno <genadimumladze44@gmail.com>
2025-04-07 17:38:13 +0400

    api gateway and service registry

---
+++ service-registry/src/test/java/com/example/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.serviceregistry;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
