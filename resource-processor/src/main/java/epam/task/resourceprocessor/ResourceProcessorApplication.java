811a296f0c6d84c6965e1cd7052af381cb53fced
genadigeno <genadimumladze44@gmail.com>
2025-04-07 17:41:45 +0400

    api gateway and service registry

--- resource-processor/src/main/java/epam/task/resourceprocessor/ResourceProcessorApplication.java
+++ resource-processor/src/main/java/epam/task/resourceprocessor/ResourceProcessorApplication.java
@@ -2,8 +2,10 @@ package epam.task.resourceprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ResourceProcessorApplication {
 
     public static void main(String[] args) {
--- song-service/src/main/java/epam/task/song/SongServiceApplication.java
+++ song-service/src/main/java/epam/task/song/SongServiceApplication.java
@@ -2,10 +2,10 @@ package epam.task.song;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class SongServiceApplication {
 
 	public static void main(String[] args) {
